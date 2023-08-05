d17524ed43b48e7fa9c092ba0abd9d380deb830b
ajaythapliyal <ajaythapliyal1703@gmail.com>
2023-08-05 22:12:14 +0530

    Header WIP

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,34 @@
-import Image from "next/image"
-import harbor from "../public/harbor.svg"
-import project from "../public/icons/Projects.svg"
+import Harbor from "../public/icons/harbor.svg"
+import Projects from "../public/icons/projects.svg"
+import Docs from "../public/icons/docs.svg"
+import CommandSheet from "../public/icons/command_sheet.svg"
+import UserKey from "../public/icons/user_key.svg"
+import Profile from "../public/icons/profile.svg"
 export function Header(){
-    return <header className="bg-primary-100 px-5 py-4">
-        <div className="flex gap-10">
-        <Image src={harbor}  alt="branding"/>
-        <nav className="text-secondary-200">
-            <div className="flex gap-2">
-                <Image src={project} width={14} height={14}  className="fill-green" alt="projects"/>
+    return <header className="bg-primary-100 px-5 py-2">
+        <div className="flex gap-10 items-center text-secondary-200">
+        <Harbor></Harbor>
+        <nav className="gap-3 flex">
+            <div className="flex gap-2 items-center px-4 py-2">
+                <Projects className="w-3 h-3 fill-secondary-200"></Projects>
                 <p>Projects</p>
             </div>
+            <div className="flex gap-2 items-center px-4 py-2">
+                <Docs className="w-3 h-3 fill-secondary-200"></Docs>
+                <p>Docs</p>
+            </div>
+            <div className="flex gap-2 items-center px-4 py-2">
+                <CommandSheet className="w-3 h-3 fill-secondary-200"></CommandSheet>
+                <p>Command Cheatsheet</p>
+            </div>
         </nav>
+        <div className="flex grow justify-end gap-5">
+            <div className="flex gap-2 items-center px-4 py-2">
+                <UserKey className="w-3 h-3 fill-secondary-200"></UserKey>
+                <p>Your user key</p>
+            </div>
+            <Profile></Profile>
+        </div>
         </div>
         
     </header>
